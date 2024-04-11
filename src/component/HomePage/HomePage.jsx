c7a736b540824c4a2ed55cfdd8c2a6d10fc88685
anuragK-24 <24sept.anurag@gmail.com>
2024-04-11 13:36:51 +0530

    Added the homepage style

--- src/component/HomePage/HomePage.jsx
+++ src/component/HomePage/HomePage.jsx
@@ -1,19 +1,22 @@
 import { Link } from "react-router-dom";
 import DataJson from "../../DataJson/DataJson";
+import "./HomePage.scss";
 export default function HomePage() {
   const puja = Object.keys(DataJson);
   return (
-    <div>
-      <h1>Home Page</h1>
-      <h2>Available Pooja</h2>
+    <div className="HomePage">
+      <h1 className="HomePage__Heading">Home Page</h1>
+      <h2 className="HomePage__SubHeading">Available Pooja</h2>
       <hr />
-      <ul>
-        {puja.map((pooja, index) => (
-          <li key={index}>
-            <Link to={`/puja/${pooja}`}>{pooja}</Link>
-          </li>
-        ))}
-      </ul>
+      <div className="HomePage__Content">
+        <ol>
+          {puja.map((pooja, index) => (
+            <li key={index}>
+              <Link to={`/puja/${pooja}`}>{pooja}</Link>
+            </li>
+          ))}
+        </ol>
+      </div>
     </div>
   );
 }
